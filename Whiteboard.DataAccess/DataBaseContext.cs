5cab57630fe66a702d636b6a53142aa409662d28
D'jalmar Gutierrez Titirico <dajaman1@hotmail.com>
2014-02-13 18:54:42 -0400

    capas

---
+++ Whiteboard.DataAccess/DataBaseContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whiteboard.DataAccess
+{
+    public class DataBaseContext:DbContext,IDisposable
+    {
+        private static DataBaseContext context = new DataBaseContext();
+
+        public static DataBaseContext Context
+        {
+            get { return context; }
+        }
+
+        private DataBaseContext()
+        {
+
+        }
+        static  DataBaseContext()
+        {
+            context.Database.CreateIfNotExists();
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ Whiteboard.DataAccess/Repositories/GenericRepository.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whiteboard.DataAccess.Repositories
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        internal DataBaseContext context;
+        internal DbSet<T> dbSet;
+
+        public GenericRepository()
+        {
+            this.context = DataBaseContext.Context;
+            this.dbSet = context.Set<T>();
+        }
+
+        public virtual T Get(long id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public virtual IEnumerable<T> Filter(Expression<Func<T, bool>> filter = null,
+                                     Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null)
+        {
+            IQueryable<T> query = this.dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+        }
+
+        public virtual T Insert(T item)
+        {
+            T newEntry = this.dbSet.Add(item);
+            this.context.SaveChanges();
+            return newEntry;
+        }
+
+        public virtual int Delete(T item)
+        {
+            if (context.Entry(item).State == System.Data.EntityState.Detached)
+            {
+                this.dbSet.Attach(item);
+            }
+            dbSet.Remove(item);
+            return context.SaveChanges();
+        }
+
+        public virtual int Update(T item)
+        {
+            if (item == null)
+            {
+                throw new ApplicationException("Null item not alowed");
+            }
+            dbSet.Attach(item);
+            context.Entry(item).State = System.Data.EntityState.Modified;
+            return context.SaveChanges();
+        }
+
+        public void SaveChanges()
+        {
+            this.context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
