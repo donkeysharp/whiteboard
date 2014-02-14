47f05da4fef933c6b93da86f6efb08532752a4c2
Doneyksharp <serguimant@gmail.com>
2014-02-14 00:07:08 -0400

    Working with MySql, package needed: MySql.Data Version: 6.7.4 and MySql.Data.Entity Version: 6.7.4.1

--- Whiteboard.DataAccess/DataBaseContext.cs
+++ Whiteboard.DataAccess/DataBaseContext.cs
@@ -5,28 +5,24 @@ using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Whiteboard.DataAccess.Models;
 
-namespace Whiteboard.DataAccess
-{
-    public class DataBaseContext:DbContext,IDisposable
-    {
+namespace Whiteboard.DataAccess {
+    public class DataBaseContext : DbContext, IDisposable {
         private static DataBaseContext context = new DataBaseContext();
+        public DbSet<Foo> Foos { get; set; }
 
-        public static DataBaseContext Context
-        {
+        public static DataBaseContext Context {
             get { return context; }
         }
 
-        private DataBaseContext()
-        {
+        private DataBaseContext() {
 
         }
-        static  DataBaseContext()
-        {
+        static DataBaseContext() {
             context.Database.CreateIfNotExists();
         }
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
     }
--- Whiteboard.Web/Areas/WebAPI/Controllers/FooController.cs
+++ Whiteboard.Web/Areas/WebAPI/Controllers/FooController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Whiteboard.DataAccess;
 
 namespace Whiteboard.Web.Areas.WebAPI.Controllers {
     public class FooController : Controller {
         // GET: /api/Foo/
         public string Index() {
+            var a = Whiteboard.DataAccess.DataBaseContext.Context;
             return "Foo";
         }
     }
