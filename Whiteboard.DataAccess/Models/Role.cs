bc9f6f74b03ebbd5b339536bf4e70a7c53f26ee9
D'jalmar Gutierrez Titirico <dajaman1@hotmail.com>
2014-02-26 20:21:31 -0400

    Created Role and RoleProfile models, no changes in Profile model

--- Whiteboard.DataAccess/Models/Role.cs
+++ Whiteboard.DataAccess/Models/Role.cs
@@ -6,18 +6,13 @@ using System.Threading.Tasks;
 
 namespace Whiteboard.DataAccess.Models
 {
-    public class Profile
+    public class Role
     {
         public const string ROLE_SCHOOL = "Profile.SchoolRole";
         public const string ROLE_TEACHER = "Profile.TeacherRole";
         public const string ROLE_STUDENT = "Profile.StudentRole";
 
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string Country { get; set; }
-        public string PictureUrl { get; set; }
-        public String Role { get; set; }
+        public string Value { get; set; }
     }
 }
---
+++ Whiteboard.DataAccess/Models/RoleProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whiteboard.DataAccess.Models
+{
+    public class RoleProfile
+    {
+        public int Id { get; set; }
+
+        public int RoleID { get; set; }
+        [ForeignKey("RoleID")]
+        public Role Role { get; set; }
+
+        public int ProfileID { get; set; }
+        [ForeignKey("ProfileID")]
+        public Profile Profile { get; set; }
+    }
+}
