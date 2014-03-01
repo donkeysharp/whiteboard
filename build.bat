e5cebad3d8e19618cb47252ae8da3c473f0784d6
donkeysharp <serguimant@gmail.com>
2014-03-01 01:05:47 -0400

    build.bat script can create a package and deploy it on IIS

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
 echo "Building ..."
-%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Whiteboard.sln /p:Configuration=Release /p:Platform="Any CPU"
+rem %windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Whiteboard.sln /p:Configuration=Debug /p:Platform="Any CPU"
+%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Whiteboard.sln /p:DeployOnBuild=true /p:PublishProfile=pizarron_profile.pubxml
 
+rem "%ProgramFiles%\IIS\Microsoft Web Deploy V3\msdeploy.exe" -verb:sync -source:package=deploy_package\Whiteboard.Web.zip -dest:auto -setParam:kind=ProviderPath,scope=Pizarron,value=pizarron.test
 
-
+"%ProgramFiles%\IIS\Microsoft Web Deploy V3\msdeploy.exe" -verb:sync -source:package=deploy_package\Whiteboard.Web.zip -dest:auto
