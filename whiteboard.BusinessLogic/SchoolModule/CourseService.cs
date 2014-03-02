b885daf52e939d2fa9ca3db0c217efd2d60f8242
D'jalmar Gutierrez Titirico <dajaman1@hotmail.com>
2014-03-02 03:57:12 -0400

    Search course method in service

---
+++ whiteboard.BusinessLogic/SchoolModule/CourseService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Whiteboard.DataAccess.Models;
+using Whiteboard.DataAccess.Repositories;
+
+namespace whiteboard.BusinessLogic.SchoolModule
+{
+    public class CourseService : GenericService<Course>, ICourseService
+    {
+        private CourseService(ICourseRepository da)
+            : base(da)
+        {
+
+        }
+        public static ICourseService GetInstance<T>() where T : ICourseRepository
+        {
+            ICourseRepository da = (ICourseRepository)Activator.CreateInstance<T>();
+            return new CourseService(da);
+        }
+
+        public IEnumerable<Course> Search(string data)
+        {
+            //add more search parameters ???
+            return da.Filter(
+                x => (x.Description.Contains(data) ||
+                x.Title.Contains(data) ||
+                x.Syllabus.Contains(data))
+                );
+        }
+    }
+}
