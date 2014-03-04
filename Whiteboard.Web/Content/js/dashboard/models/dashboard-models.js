178b9754d6b2870bf359e861681a39ae92d6b5b3
D'jalmar Gutierrez Titirico <dajaman1@hotmail.com>
2014-03-03 21:12:38 -0400

    teacher and student models and collection added

--- Whiteboard.Web/Content/js/dashboard/models/dashboard-models.js
+++ Whiteboard.Web/Content/js/dashboard/models/dashboard-models.js
@@ -5,7 +5,23 @@
         "Description": ""
     }
 });
+TeacherItemModel = Backbone.Model.extend({
+    defaults: {
+        "PictureUrl": "",
+        "Name":""
+    }
+});
+StudentItemModel = Backbone.Model.extend({
+    defaults: {
+        "PictureUrl": "",
+        "Name": ""
+    }
+});
 CourseCollection = Backbone.Collection.extend({
     url: '/dashboard/mycourses',
     model: CourseItemModel,
+});
+TeacherCollection = Backbone.Collection.extend({
+    url: '/dashboard/teachers',
+    model: TeacherItemModel,
 });
