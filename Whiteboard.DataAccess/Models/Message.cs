edd693acd33dbedce3e2c714afd7acf468d2d7ed
D'jalmar Gutierrez Titirico <dajaman1@hotmail.com>
2014-03-08 17:07:10 -0400

    message model added

---
+++ Whiteboard.DataAccess/Models/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whiteboard.DataAccess.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+
+        public int SenderId { get; set; }
+        [ForeignKey("SenderId")]
+        public Profile Sender { get; set; }
+
+        public int CourseId { get; set; }
+        [ForeignKey("CourseId")]
+        public Course Course { get; set; }
+
+    }
+}
--- Whiteboard.Web/Areas/WebAPI/Controllers/FooController.cs
+++ Whiteboard.Web/Areas/WebAPI/Controllers/FooController.cs
@@ -4,13 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Whiteboard.DataAccess;
+using Whiteboard.DataAccess.Models;
 
 namespace Whiteboard.Web.Areas.WebAPI.Controllers {
     public class FooController : Controller {
         // GET: /api/Foo/
-        public string Index() {
-            var a = Whiteboard.DataAccess.DataBaseContext.Context;
-            return "Foo";
+        public JsonResult Index() {
+            Message message=new Message()
+            {
+                Id=12312312,
+                CourseId=1,
+                SenderId=2
+            };
+            return Json(message, JsonRequestBehavior.AllowGet);
         }
+
     }
 }
