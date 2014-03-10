dc903c236c3bdc19471e713d0e990ceaace4ad57
donkeysharp <serguimant@gmail.com>
2014-03-10 11:25:10 -0400

    server build fix

--- build.server.bat
+++ build.server.bat
@@ -1,6 +1,6 @@
 echo "Building ..."
 rem %windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Whiteboard.sln /p:Configuration=Debug /p:Platform="Any CPU"
-%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Whiteboard.sln /p:DeployOnBuild=true /p:PublishProfile=pizarron_profile.pubxml
+%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Whiteboard.sln /p:DeployOnBuild=true /p:PublishProfile=pizarron_profile-server.pubxml
 
 rem "%ProgramFiles%\IIS\Microsoft Web Deploy V3\msdeploy.exe" -verb:sync -source:package=deploy_package\Whiteboard.Web.zip -dest:auto -setParam:kind=ProviderPath,scope=Pizarron,value=pizarron.test
 
