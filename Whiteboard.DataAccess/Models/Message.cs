327af9dffa0a3da8966ce2b4b04d78f6d9e63a05
D'jalmar Gutierrez Titirico <dajaman1@hotmail.com>
2014-03-13 19:42:02 -0400

    chat and message services

--- Whiteboard.DataAccess/Models/Message.cs
+++ Whiteboard.DataAccess/Models/Message.cs
@@ -11,13 +11,12 @@ namespace Whiteboard.DataAccess.Models
     {
         public int Id { get; set; }
 
-        public int SenderId { get; set; }
-        [ForeignKey("SenderId")]
-        public Profile Sender { get; set; }
+        public string Message { get; set; }
+        public string UserName { get; set; }
 
-        public int CourseId { get; set; }
-        [ForeignKey("CourseId")]
-        public Course Course { get; set; }
+        public int CourseClassId { get; set; }
+        [ForeignKey("CourseClassId")]
+        public CourseClass CourseClass { get; set; }
 
     }
 }
---
+++ whiteboard.BusinessLogic/SchoolModule/MessageService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Whiteboard.DataAccess.Models;
+using Whiteboard.DataAccess.Repositories;
+
+namespace whiteboard.BusinessLogic.SchoolModule
+{
+    public class MessageService : GenericService<Message>, IMessageService
+    {
+        private MessageService(IMessageRepository da):base(da)
+        {
+
+        }
+
+        public static IMessageService GetInstance<T>() where T : IMessageRepository
+        {
+            IMessageRepository da = (IMessageRepository)Activator.CreateInstance<T>();
+            return new MessageService(da);
+        }
+
+        public IEnumerable<Message> GetMessagesByClassId(int CourseClassId)
+        {
+            return da.Filter(x => x.CourseClassId == CourseClassId);
+        }
+    }
+}
